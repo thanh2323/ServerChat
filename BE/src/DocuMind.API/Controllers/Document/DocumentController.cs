cfabb5bf74f00bfa2120efb524fc761f230199ef
Chi Thanh <nguyenchithanh2213@gmail.com>
2025-12-18 15:33:45 +0700

    feature: add controller document for upload

---
+++ BE/src/DocuMind.API/Controllers/Document/DocumentController.cs
@@ -0,0 +1,37 @@
+﻿using DocuMind.Application.DTOs.Common;
+using System.Security.Claims;
+using DocuMind.Application.DTOs.Document;
+using DocuMind.Application.Interface.IDocument;
+using DocuMind.Application.Services.UserService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Google.GenAI;
+
+namespace DocuMind.API.Controllers.Document
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DocumentController : ControllerBase
+    {
+        private readonly IDocumentService _documentService;
+
+        public DocumentController(IDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadDocument([FromForm] UploadDocumentDto dto)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+          
+            var result = await _documentService.UploadDocument(dto, int.Parse(userId!));
+
+            if (!result.Success)
+                return BadRequest(ApiResponse<DocumentItemDto>.ErrorResponse(result.Message));
+
+            return Ok(result.Data);
+        }
+    }
+}
