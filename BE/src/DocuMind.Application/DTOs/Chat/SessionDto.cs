74233a828e7ce5d95ffa5d3fd18c031a79f7b4fc
Chi Thanh <nguyenchithanh2213@gmail.com>
2025-12-22 16:49:29 +0700

    update code

---
+++ BE/src/DocuMind.Application/DTOs/Chat/SessionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuMind.Application.DTOs.Chat
+{
+    public class SessionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastActiveAt { get; set; }
+        public int MessageCount { get; set; }
+        public List<SessionDocumentDto> Documents { get; set; } = new();
+    }
+}
